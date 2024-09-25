8a42765c5b13f0281096d8b8f7d47ddf8807f413
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-09-25 00:13:01 -0300

    Update Atividade2Struct.cpp

--- Atividades Struct/Atividade2Struct.cpp
+++ Atividades Struct/Atividade2Struct.cpp
@@ -1,98 +1,163 @@
 #include <stdio.h>
+#include <stdbool.h> // Para utilizar o tipo bool
 
-struct Entrada{
-    int valor;
+struct Entrada {
+    int grupoLendo[10];
 
-    void capturar(){
-        printf("Digite os numeros:\n");
-        for(int i = 0; i < 10; i++) {
-            scanf("%d", &valor);
-        }printf("\n");
-    }
-};
-
-struct Grupo{
-    int elementos[20];
-    int cont = 0;
-    Entrada numeros[20];
-
-    void adicionar(){
-        if(cont < 20){
-            numeros[cont].capturar();
-            cont++;
-            printf("Grupo criado: %d\n\n", cont);
-        }else {
-            printf("Nao existem mais espacos para criar grupos.\n\n");
-        }
-    }
-
-    void obter(){
-        if(cont == 0) {
-            printf("Nao ha grupos preenchidos.\n\n");
-        }else {
-            printf("Escolha um grupo para ler:\n");
-            for(int i = 0; i < cont; i++) {
-                printf("%d - ", i);
-            }
-            int pos;
-            scanf("%d%*c", &pos);
-            numeros[pos].capturar();
-        }
-    }
-
-    void juntar(){
-        printf("Juntando dois grupos...\n");
-    }
-
-    void interseccao(){
-        printf("Fazendo interseção...\n");
-    }
-
-    void imprimir(){
-        printf("Imprimindo grupos:\n");
-        for(int i = 0; i < cont; i++) {
-            printf("Grupo %d: %d\n", i, numeros[i].valor);
+    void capturar(int indice) {
+        printf("Digite os numeros para o grupo %d:\n", indice + 1);
+        for (int i = 0; i < 10; i++) {
+            scanf("%d", &grupoLendo[i]);
         }
         printf("\n");
     }
 };
 
-struct Menu{
-    int operacao;
-    Grupo grupo;
+struct Grupo {
+    int cont;
+    Entrada numeros[20];
 
-    void mostrar(){
+    Grupo() : cont(0) {}
+
+    void obter(int qtdGrupoVazio){
+        if(qtdGrupoVazio == 0){
+            printf("Nao ha grupos preenchidos.\n\n");
+        }else{
+            printf("Escolha um grupo para ler:\n");
+            for(int i = 0; i < qtdGrupoVazio; i++) {
+                printf("%d - Grupo %d\n", i, i + 1);
+            }
+            int pos;
+            scanf("%d", &pos);
+            if(pos >= 0 && pos < qtdGrupoVazio){
+                numeros[pos].capturar(pos);
+            }else {
+                printf("Grupo invalido.\n");
+            }
+        }
+    }
+
+    void juntar(int qtdGrupoVazio){
+        if(qtdGrupoVazio < 2){
+            printf("Nao ha grupos suficientes para realizar a uniao.\n\n");
+        }else{
+            printf("Escolha dois grupos para uniao:\n");
+            for (int i = 0; i < qtdGrupoVazio; i++) {
+                printf("%d - Grupo %d\n", i, i + 1);
+            }
+            int posicao1, posicao2;
+            scanf("%d %d", &posicao1, &posicao2);
+
+            if(posicao1 >= 0 && posicao1 < qtdGrupoVazio && posicao2 >= 0 && posicao2 < qtdGrupoVazio){
+                printf("Unindo grupos %d e %d\n", posicao1 + 1, posicao2 + 1);
+                for (int i = 0; i < 10; i++){
+                    printf("%d ", numeros[posicao1].grupoLendo[i]);
+                }
+                for(int i = 0; i < 10; i++){
+                    printf("%d ", numeros[posicao2].grupoLendo[i]);
+                }
+                printf("\nGrupos unidos com sucesso.\n");
+            }else {
+                printf("Grupos invalidos.\n");
+            }
+        }
+    }
+
+    void interseccao(int qtdGrupoVazio){
+        if (qtdGrupoVazio < 2){
+            printf("Nao ha grupos suficientes para realizar a interseccao.\n\n");
+            return;
+        }
+
+        printf("Escolha dois grupos para interseccao:\n");
+        for(int i = 0; i < qtdGrupoVazio; i++) {
+            printf("%d - Grupo %d\n", i, i + 1);
+        }
+        int posicao1, posicao2;
+        scanf("%d %d", &posicao1, &posicao2);
+
+        if(posicao1 >= 0 && posicao1 < qtdGrupoVazio && posicao2 >= 0 && posicao2 < qtdGrupoVazio){
+            printf("Fazendo interseção entre grupos %d e %d...\n", posicao1 + 1, posicao2 + 1);
+            Entrada resultado;
+            int tamanhoResultado = 0;
+
+            for(int i = 0; i < 10; i++){
+                for (int j = 0; j < 10; j++){
+                    if(numeros[posicao1].grupoLendo[i] == numeros[posicao2].grupoLendo[j]){
+                        bool existe = false;
+                        for(int k = 0; k < tamanhoResultado; k++){
+                            if(resultado.grupoLendo[k] == numeros[posicao1].grupoLendo[i]){
+                                existe = true;
+                                break;
+                            }
+                        }
+                        if(!existe){
+                            resultado.grupoLendo[tamanhoResultado++] = numeros[posicao1].grupoLendo[i];
+                        }
+                    }
+                }
+            }
+            printf("Interseção resultante:\n");
+            for (int i = 0; i < tamanhoResultado; i++) {
+                printf("%d ", resultado.grupoLendo[i]);
+            }
+            printf("\n");
+        } else {
+            printf("Grupos invalidos.\n");
+        }
+    }
+
+    void imprimir(){
+        printf("Imprimindo grupos:\n");
+        for (int i = 0; i < cont; i++){
+            printf("Grupo %d: ", i + 1);
+            for (int j = 0; j < 10; j++){
+                printf("%d ", numeros[i].grupoLendo[j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+};
+
+int main(){
+    Grupo grupo;
+    int operacao = 1;
+    int qtdGrupoVazio = 0;
+
+    while(operacao != 0){
         printf("Escolha uma operacao:\n");
-        printf("Digite 1 - Criar um grupo.\n");
+        printf("Digite 1 - Criar um grupo vazio.\n");
         printf("Digite 2 - Ler dados de um grupo.\n");
         printf("Digite 3 - Juntar dois grupos.\n");
         printf("Digite 4 - Fazer interseção de dois grupos.\n");
         printf("Digite 5 - Imprimir um grupo.\n");
         printf("Digite 0 - Fechar sistema.\n");
-        scanf("%d%*c", &operacao);
-    }
+        scanf("%d", &operacao);
 
-    void executar(){
-        while(operacao != 0){
-            mostrar();
-            if(operacao == 1){
-                grupo.adicionar();
-            }else if(operacao == 2){
-                grupo.obter();
-            }else if(operacao == 3){
-                grupo.juntar();
-            }else if(operacao == 4){
-                grupo.interseccao();
-            }else if(operacao == 5){
-                grupo.imprimir();
+        if(operacao == 1){
+            if (qtdGrupoVazio < 20){
+                grupo.numeros[qtdGrupoVazio] = Entrada();
+                qtdGrupoVazio++;
+                grupo.cont++;
+                printf("Grupo vazio criado.\n");
+            }else {
+                printf("Limite de grupos atingido.\n");
             }
+        }else if (operacao == 2){
+            grupo.obter(qtdGrupoVazio);
+        }else if (operacao == 3){
+            grupo.juntar(qtdGrupoVazio);
+        }else if (operacao == 4){
+            grupo.interseccao(qtdGrupoVazio);
+        }else if (operacao == 5){
+            grupo.imprimir();
+        }else if (operacao == 0){
+            printf("Fechando o sistema...\n");
+        }else {
+            printf("Opcao invalida.\n");
         }
     }
-};
 
-int main(){
-    Menu menu;
-    menu.operacao = 1;
-    menu.executar();
     return 0;
 }
