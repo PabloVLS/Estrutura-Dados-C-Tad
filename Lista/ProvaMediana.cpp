39d3389f418cc4ebf350004d037392fd8e405006
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-10-31 20:20:49 -0300

    Update ProvaMediana.cpp

--- Lista/ProvaMediana.cpp
+++ Lista/ProvaMediana.cpp
@@ -1,90 +1,101 @@
-#include <iostream>
+#include<stdio.h>
 
-struct Node{
-  Node* next;
-  Node* prev;
-  double value;
+struct Node {
 
-  Node(){
-      value =0.0;
-      next=NULL;
-  }
+    int value;
+    Node* next;
 
-  Node(double _value) {
+    Node() { // O(1)
+        value = 0;
+        next = NULL;
+    }
+
+    Node(int _value) { // O(1)
         value = _value;
         next = NULL;
-  }
+    }
 
 };
 
-struct List{
+struct List {
+
     Node* first;
-    Node* mid;
     Node* last;
     int c;
+    Node* mid;
 
-    List(){
+    List() { // O(1)
         first = NULL;
         last = NULL;
-        c=0;
+        c = 0;
+        mid=NULL;
     }
 
-    bool empty(){
+
+    bool empty() { // O(1)
         return first == NULL;
     }
 
-    void pushBack(double value){
+
+
+    void pushBack(int value) { // O(1)
         Node *n = new Node(value);
         c++;
-        if(empty()){
+        if (empty()) {
             first = n;
-            last=n;
+            last = n;
             mid=n;
-            print();
-            printf("%.2lf\n", value);
             return;
         }
-        n->prev = last;
         last->next = n;
         last = n;
-
-
-
-        if(c%2==0){
-            mid = mid->next;
-        }
-        print();
-
-         if (c % 2 == 0) {
-            double media = (mid->value + mid->prev->value) / 2;
-            printf("%.2lf\n", media);
-        } else {
-            printf("%.2lf\n", mid->value);
+        if(c%2!=0){
+            mid=mid->next;
         }
     }
 
-    void print(){
+    void print() {
         Node* aux = first;
-        while(aux != NULL){
-            printf("%.0lf ",aux->value);
+        while (aux != NULL) {
+            printf("%d ", aux->value);
             aux = aux->next;
         }
-
     }
 
+
+
+    int size() { // O(1)
+        return c;
+    }
+
+
+    void calcula(){
+        print();
+        if(c%2==0){
+            double val=(mid->value + mid->next->value)/2.0;
+            printf("%.2lf\n",val);
+            return;
+        }else{
+            printf("%.2lf\n",double(mid->value));
+
+        }
+    }
 
 };
 
+int main() {
 
-int main()
-{
-    int qtd;
-    double numero;
     List l;
+
+    int qtd;
+
     scanf("%d",&qtd);
-    for(int i=0 ; i< qtd;i++){
-        scanf("%lf",&numero);
-        l.pushBack(numero);
+
+    for(int i=0;i<qtd;i++){
+        int num;
+        scanf("%d",&num);
+        l.pushBack(num);
+        l.calcula();
     }
 
     return 0;
